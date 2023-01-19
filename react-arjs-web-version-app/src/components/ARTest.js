6af10a891bdb96a5f38dde7f9fe3850e17a3c3d8
Ava Mirzaee Cheshmeh <avamc@avas-air.home>
2023-01-19 17:35:14 +0100

    going back to testing image, since nothing seems to work right now... for #4 and #6

--- react-arjs-web-version-app/src/components/ARTest.js
+++ react-arjs-web-version-app/src/components/ARTest.js
@@ -1,25 +1,24 @@
-import { ARCanvas, ARMarker } from "@artcom/react-three-arjs";
+import { ARCanvas, ARMarker, ARNft } from "@artcom/react-three-arjs";
 
 const ARTest = ({}) => {
-    return (
-        <ARCanvas
-        style={{zIndex:10}}
-    camera={ { position: [0, 0, 0] } }
-    onCreated={ ({ gl }) => {
-      gl.setSize(window.innerWidth, window.innerHeight)
-    } }>
-    <ambientLight />
-    <pointLight position={ [10, 10, 0] }  />
-    <ARMarker
-      type={ "pattern" }
-      patternUrl={ "data/pattern-test-3.patt" }>
-      <mesh>
-        <boxGeometry args={ [1, 1, 1] } />
-        <meshStandardMaterial color={ "green" } />
-      </mesh>
-    </ARMarker>
-  </ARCanvas>
-    )
-}
+  return (
+    <ARCanvas
+      style={{ zIndex: 10 }}
+      camera={{ position: [0, 0, 0] }}
+      onCreated={({ gl }) => {
+        gl.setSize(window.innerWidth, window.innerHeight);
+      }}
+    >
+      <ambientLight />
+      <pointLight position={[10, 10, 0]} />
+      <ARMarker type={"pattern"} patternUrl={"data/hiro.patt"}>
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color={"green"} />
+        </mesh>
+      </ARMarker>
+    </ARCanvas>
+  );
+};
 
 export default ARTest;
