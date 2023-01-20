1db8da707b24059768c8ff0e8c3077bcb2e86551
Ava Mirzaee Cheshmeh <avamc@avas-air.home>
2023-01-20 17:46:51 +0100

    trying and failing to make AR js work in react for #6

--- nft-react-test/src/components/ARTest.js
+++ nft-react-test/src/components/ARTest.js
@@ -1,46 +1,46 @@
 import 'aframe';
 import '@ar-js-org/ar.js';
 
-// Setup for a-frame
-AFRAME.registerComponent('samplehandler', {
-    init: function () {
-        console.log("samplehandler init...")
-        let marker = this.el;
-        //let marker = this.el.sceneEl;
+// // Setup for a-frame
+// AFRAME.registerComponent('samplehandler', {
+//     init: function () {
+//         console.log("samplehandler init...")
+//         let marker = this.el;
+//         //let marker = this.el.sceneEl;
 
-        marker.addEventListener('markerFound', function () {
-            console.log("markerFound...");
-        }.bind(this));
+//         marker.addEventListener('markerFound', function () {
+//             console.log("markerFound...");
+//         }.bind(this));
 
-        marker.addEventListener('markerLost', function() {
-            console.log("markerLost...");
-        }.bind(this));
-    }
-});
+//         marker.addEventListener('markerLost', function() {
+//             console.log("markerLost...");
+//         }.bind(this));
+//     }
+// });
 
-AFRAME.registerComponent('modify-materials', {
-    init: function () {
-        let tex = new THREE.TextureLoader().load('/assets/Map-COL.jpg');
+// AFRAME.registerComponent('modify-materials', {
+//     init: function () {
+//         let tex = new THREE.TextureLoader().load('/assets/Map-COL.jpg');
 
-        // Wait for model to load.
-        this.el.addEventListener('model-loaded', () => {
-            // Grab the mesh / scene.
-            const obj = this.el.getObject3D('mesh');
-            // Go over the submeshes and modify materials we want.
-            obj.traverse(node => {
-                console.log("node.name:", node.name);
-                if (node.name == "LeePerrySmith") {
-                    // node.material.color.set('red');
-                    node.material.map = tex;
-                }
-            });
-        });
-    }
-});
+//         // Wait for model to load.
+//         this.el.addEventListener('model-loaded', () => {
+//             // Grab the mesh / scene.
+//             const obj = this.el.getObject3D('mesh');
+//             // Go over the submeshes and modify materials we want.
+//             obj.traverse(node => {
+//                 console.log("node.name:", node.name);
+//                 if (node.name == "LeePerrySmith") {
+//                     // node.material.color.set('red');
+//                     node.material.map = tex;
+//                 }
+//             });
+//         });
+//     }
+// });
 
-window.onload = function() {
-    console.log("window loaded...");
-};
+// window.onload = function() {
+//     console.log("window loaded...");
+// };
 
 
 const ARTest = () => {
--- react-arjs-web-version-app/src/components/ARTest.js
+++ react-arjs-web-version-app/src/components/ARTest.js
@@ -1,4 +1,12 @@
 import { ARCanvas, ARMarker} from "@artcom/react-three-arjs";
+// import { AMarkerCamera } from "aframe";
+// extend({ AMarkerCamera });
+// import { extend } from "@react-three/fiber";
+// import "arjs";
+// import '@ar-js-org/ar.js';
+// import "aframe";
+// require("aframe");
+
 
 const ARTest = ({}) => {
   return (
@@ -13,16 +21,59 @@ const ARTest = ({}) => {
       <pointLight position={[10, 10, 0]} />
       <ARMarker
         type={"pattern"}
-        patternUrl={"data/pattern-test-2.patt"}
+        patternUrl={"data/hiro.patt"}
         onMarkerfound={() => console.log("found marker")}
         onMarkerlost={() => console.log("lost marker")}
+        preset={"custom"}
       >
+      {/* <aMarkerCamera preset="hiro" type="pattern" url="data/hiro.patt"> */}
         <mesh>
           <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color={"green"} />
         </mesh>
+      {/* </aMarkerCamera> */}
       </ARMarker>
     </ARCanvas>
+    // <>
+    //   {/* <a-scene embedded arjs>
+    //     <a-marker preset="hiro">
+    //       <a-entity
+    //         position="0 0 0"
+    //         scale="0.05 0.05 0.05"
+    //         gltf-model="assets/tracking/Buggy0.gltf"
+    //       ></a-entity>
+    //     </a-marker>
+    //     <a-entity camera></a-entity>
+    //   </a-scene> */}
+    //   <a-scene
+    //     embedded
+    //     arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;"
+    //   >
+    //     {/* <a-assets>
+    //       <a-asset-item
+    //         id="animated-asset"
+    //         src="assets/tracking/Buggy0.gltf"
+    //       ></a-asset-item>
+    //     </a-assets> */}
+
+    //     <a-marker type="barcode" value="7">
+    //       <a-box position="0 0.5 0" color="yellow"></a-box>
+    //     </a-marker>
+
+    //     <a-marker preset="hiro" url="data/hiro.patt" type="pattern" value="6">
+    //       {/* <a-entity
+    //             gltf-model="#animated-asset"
+    //             scale="2">
+    //         </a-entity> */}
+    //       <a-entity
+    //         position="0 0 0"
+    //         scale="0.05 0.05 0.05"
+    //         gltf-model="assets/tracking/Buggy0.gltf"
+    //       ></a-entity>
+    //     </a-marker>
+    //     <a-entity camera></a-entity>
+    //   </a-scene>
+    // </>
   );
 };
 
